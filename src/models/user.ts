9ee1bef19131ddff10abbb5079d4044eba00cec1
ShlomoNus <shlomonuswork@gmail.com>
2024-02-06 22:11:48 +0200

    only email unique

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,7 +7,7 @@ interface IUser extends Document {
 }
 
 const UserSchema = new Schema<IUser>({
-    name: { type: 'String', required: true, unique: true },
+    name: { type: 'String', required: true },
     password: { type: 'String', required: true },
     email: { type: 'String', required: true, unique: true },
 });
