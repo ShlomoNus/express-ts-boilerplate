a9166134b4d97b812bf58019b7bbb7dbaa488552
ShlomoNus <shlomonuswork@gmail.com>
2024-02-06 18:55:44 +0200

    add use mongoose model

---
+++ src/models/user.ts
@@ -0,0 +1,24 @@
+import { model, Schema, Document, Model } from 'mongoose';
+
+interface IUser extends Document {
+    name: string;
+    password: string;
+    email: string;
+}
+
+const UserSchema = new Schema<IUser>({
+    name: { type: 'String', required: true, unique: true },
+    password: { type: 'String', required: true },
+    email: { type: 'String', required: true, unique: true },
+});
+
+interface IUserModel extends Model<IUser> {
+    findByTitle(title: string): Promise<IUser | null>;
+}
+UserSchema.statics.findByEmail = function findByEmail(
+    email: string
+): Promise<IUser | null> {
+    return this.findOne({ email }).exec();
+};
+
+export const UserModel = model<IUser, IUserModel>('User', UserSchema);
