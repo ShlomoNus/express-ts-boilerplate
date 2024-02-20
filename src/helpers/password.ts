f6336dfea92ee33e18a3e3e4960fbcc94e906e66
ShlomoNus <shlomonuswork@gmail.com>
2024-02-20 11:50:44 +0200

    add hash password function

---
+++ src/helpers/password.ts
@@ -0,0 +1,9 @@
+import { genSalt, hash } from 'bcrypt';
+
+export async function hashPassword(password: string) {
+    const salt = await genSalt(10);
+
+    const hashedPassword = await hash(password, salt);
+
+    return hashedPassword;
+}
