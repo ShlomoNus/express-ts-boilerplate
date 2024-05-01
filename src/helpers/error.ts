ef45aaf59b0fdb71d8068ced9a39a7050eff8a6d
ShlomoNus <shlomonuswork@gmail.com>
2024-05-01 16:56:32 +0300

    feature: create error classes and getter function

---
+++ src/helpers/error.ts
@@ -0,0 +1,63 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable @typescript-eslint/no-useless-constructor */
+
+import { StatusCodes } from 'http-status-codes';
+
+type DefaultErrorConstractorInputObject = {
+    message: string;
+    statusCode: number;
+};
+
+export class CustomAPIError extends Error {
+    statusCode: number;
+
+    constructor({ message, statusCode }: DefaultErrorConstractorInputObject) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+export class BadRequestError extends CustomAPIError {
+    constructor(message: string) {
+        super({ message, statusCode: StatusCodes.BAD_REQUEST });
+    }
+}
+
+export class ConflictError extends CustomAPIError {
+    constructor(message: string) {
+        super({ message, statusCode: StatusCodes.CONFLICT });
+    }
+}
+
+export class NotFoundError extends CustomAPIError {
+    constructor(message: string) {
+        super({ message, statusCode: StatusCodes.NOT_FOUND });
+    }
+}
+
+export class UnauthenticatedError extends CustomAPIError {
+    constructor(message: string) {
+        super({ message, statusCode: StatusCodes.UNAUTHORIZED });
+    }
+}
+
+export class UnauthorizedError extends CustomAPIError {
+    constructor(message: string) {
+        super({ message, statusCode: StatusCodes.FORBIDDEN });
+    }
+}
+
+export const errorsObject = {
+    BadRequestError,
+    ConflictError,
+    NotFoundError,
+    UnauthenticatedError,
+    UnauthorizedError,
+};
+
+type ErrorsNames = keyof typeof errorsObject;
+
+export function getErrorByName(errorsName: ErrorsNames, message: string) {
+    const SelectedClass = errorsObject[errorsName];
+    return new SelectedClass(message);
+}
