d0273c150f8ef64428142d872f48b978b92617f7
ShlomoNus <shlomonuswork@gmail.com>
2024-05-08 10:09:37 +0300

    feature: add devDefault to Base url and Default_End_Point in config file

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ import { loadEnvFiles } from 'helpers/vars';
 loadEnvFiles();
 
 export const CONFIG = cleanEnv(process.env, {
-    Base_Url: url(),
-    Default_End_Point: str(),
+    Base_Url: url({ devDefault: 'mongodb://127.0.0.1:27017/' }),
+    Default_End_Point: str({ devDefault: 'Test' }),
     Test_End_Point: str(),
     Port: num({ devDefault: 8080 }),
     Secret: str({ devDefault: '12345' }),
