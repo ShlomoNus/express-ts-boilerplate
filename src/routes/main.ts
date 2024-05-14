652f1087733af58169fc6f5387b9d489a13f472d
ShlomoNus <shlomonuswork@gmail.com>
2024-05-14 12:16:08 +0300

    refactor: delete home route and handler

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,19 +1,19 @@
-import { home, login, signup } from 'handlers';
+import { check, login, signup } from 'handlers';
 import { requestLogger } from 'middleware';
 import { Route } from 'sn-types-backend';
 
 export const routes: Route[] = [
     {
-        method: 'get',
-        path: '/',
-        middleware: [],
-        handler: home,
-    },
-    {
         method: 'post',
         path: '/users',
         middleware: [],
         handler: signup,
+    },
+    {
+        method: 'post',
+        path: '/users/healthcheck',
+        middleware: [],
+        handler: check,
     },
     {
         method: 'post',
