55b82cb0af61db44fbc0348a277e946bcf5ca469
ShlomoNus <shlomonuswork@gmail.com>
2024-11-13 23:50:21 +0200

    add user function to the user repo file

--- src/db/repository/users.ts
+++ src/db/repository/users.ts
@@ -3,17 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 import { Result } from 'sn-types-general';
 import { InsertUser, SelectUser, usersTable } from '../schema';
 import { db } from '..';
-import { eq } from 'drizzle-orm';
+import { and, eq } from 'drizzle-orm';
 
-
-
-async function addUser  (newUser: InsertUser)  {
-   
-
+async function addUser(newUser: InsertUser) {
     let result: Result<string>;
 
     try {
-    await    db.insert(usersTable).values(newUser)
+        await db.insert(usersTable).values(newUser);
         result = {
             status: true,
             payload: 'User created',
@@ -29,16 +25,15 @@ async function addUser  (newUser: InsertUser)  {
         };
     }
 
-
     return result;
-};
+}
 
-async function getUser (id: number){
-
+async function getUser(id: number) {
     let result: Result<SelectUser>;
 
     try {
-      const user =  await db.select().from(usersTable).where(eq(usersTable.id,id))
+        const user = await db.select().from(usersTable).where(eq(usersTable.id, id));
+
         result = {
             status: true,
             payload: user[0],
@@ -54,6 +49,69 @@ async function getUser (id: number){
         };
     }
 
+    return result;
+}
+
+async function findUser(user: Pick<InsertUser, 'email' | 'username'>) {
+    const { email, username } = user;
+    let result: Result<SelectUser>;
+
+    try {
+        const foundUser = await db
+            .select()
+            .from(usersTable)
+            .where(and(eq(usersTable.email, email), eq(usersTable.username, username)));
+
+        result = {
+            status: true,
+            payload: foundUser[0],
+            statusCode: StatusCodes.OK,
+        };
+    } catch (error: unknown) {
+        const typedError = convertType<Error>(error);
+
+        result = {
+            status: false,
+            message: typedError.message,
+            statusCode: StatusCodes.BAD_REQUEST,
+        };
+    }
 
     return result;
-} 
+}
+
+async function validateUser(user: InsertUser) {
+    const { email, username, password } = user;
+    let result: Result<SelectUser>;
+
+    try {
+        const foundUser = await db
+            .select()
+            .from(usersTable)
+            .where(
+                and(
+                    eq(usersTable.email, email),
+                    eq(usersTable.username, username),
+                    eq(usersTable.password, password)
+                )
+            );
+
+        result = {
+            status: true,
+            payload: foundUser[0],
+            statusCode: StatusCodes.OK,
+        };
+    } catch (error: unknown) {
+        const typedError = convertType<Error>(error);
+
+        result = {
+            status: false,
+            message: typedError.message,
+            statusCode: StatusCodes.BAD_REQUEST,
+        };
+    }
+
+    return result;
+}
+
+export { addUser, getUser, findUser, validateUser };
