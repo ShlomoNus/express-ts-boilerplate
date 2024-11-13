b7c173825e4dc37ea68992444cd899bb21617dea
ShlomoNus <shlomonuswork@gmail.com>
2024-11-13 22:20:28 +0200

    add pg env vars to Config and remove mongo

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,7 @@ import { cleanEnv, str, url, num } from 'envalid';
 loadEnvFiles();
 
 export const CONFIG = cleanEnv(process.env, {
-    Mongo_Base_Url: url({ devDefault: 'mongodb://127.0.0.1:27017/' }),
-    Mongo_DB: str({ devDefault: 'test' }),
+    DATABASE_URL: url(),
     Port: num({ devDefault: 5050 }),
     Secret: str({ devDefault: '12345' }),
 });
