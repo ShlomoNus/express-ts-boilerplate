b314dff747c7f3a776687476a811f9f163e67205
ShlomoNus <shlomonuswork@gmail.com>
2024-11-21 23:21:26 +0200

    add convertToError utill

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,3 +3,7 @@ import { AnyType } from 'sn-types-general';
 export function convertType<T>(value: AnyType): T {
     return value as unknown as T;
 }
+
+export function convertToError(error: unknown) {
+    return convertType<Error>(error);
+}
