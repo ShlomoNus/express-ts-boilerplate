42e63cc51508c29e98a03ef922870996f17a07da
ShlomoNus <shlomonuswork@gmail.com>
2025-01-13 11:13:42 +0200

    rename db names vars

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ import { cleanEnv, str, url, num } from 'envalid';
 loadEnvFiles();
 
 export const CONFIG = cleanEnv(process.env, {
-    Mongo_Base_Url: url({ devDefault: 'mongodb://127.0.0.1:27017/' }),
-    Mongo_DB: str({ devDefault: 'test' }),
+    BASE_URL: url({ devDefault: 'mongodb://127.0.0.1:27017/' }),
+    DB_NAME: str({ devDefault: 'localUsers' }),
     Port: num({ devDefault: 5050 }),
     Secret: str({ devDefault: '12345' }),
 });
