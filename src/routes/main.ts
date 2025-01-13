e7c0f18df91d7aa27dde7029adbb5a7559c8c7f4
ShlomoNus <shlomonuswork@gmail.com>
2025-01-13 11:19:09 +0200

    refactor: fix routes

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,22 +1,7 @@
-import { login } from 'src/handlers/auth';
-import { signup } from '@handlers/auth';
-import { requestLogger } from '@middleware/requestLogger';
 import { userRouter } from './user';
 import { Route } from 'sn-types-backend';
 
 export const routes: Route[] = [
-    {
-        method: 'post',
-        path: '/login',
-        middleware: [requestLogger],
-        handler: login,
-    },
-    {
-        method: 'post',
-        path: '/signup',
-        middleware: [],
-        handler: signup,
-    },
     {
         method: 'use',
         path: '/users',
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,16 @@
-import { getUser } from '@handlers/auth';
+import { login, signup } from '@handlers/auth';
 import { applyRoutes } from '@utils/backend';
 import { Router } from 'express';
 import { Route } from 'sn-types-backend';
 
 const userRouter = Router();
 
-export const routes: Route[] = [{ path: '/get', method: 'get', handler: getUser }];
+export const routes: Route[] = [
+    { path: '/login', method: 'post', handler: login },
+    { path: '/signup', method: 'post', handler: signup },
+
+    // { path: '/reset', method: 'post', handler: resetPassword },
+];
 
 applyRoutes({ app: userRouter, routes });
 
