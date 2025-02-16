2892e4987b2ceab9df9fb2099f1642153e0ac28d
ShlomoNus <shlomonuswork@gmail.com>
2025-02-16 22:28:43 +0200

    delete reset path

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,8 +8,6 @@ const userRouter = Router();
 export const routes: Route[] = [
     { path: '/login', method: 'post', handler: login },
     { path: '/signup', method: 'post', handler: signup },
-
-    // { path: '/reset', method: 'post', handler: resetPassword },
 ];
 
 applyRoutes({ app: userRouter, routes });
